f600a5f7b69747ef96442665ba9e7fd451086726
Tina Huynh <tinamchuynh@gmail.com>
2022-09-17 15:00:35 -0700

    start notes for coding calculations

--- calendar.js
+++ calendar.js
@@ -1,30 +1,61 @@
+var results1 = document.querySelector(".selected-date1");
+var results2 = document.querySelector(".selected-date2");
 
-let select_date = document.querySelector(".result");
-
-$(".calendar").datepicker({
+$(".calendar1").datepicker({
     dateFormat: 'mm/dd/yy',
     firstDay: 1
 });
 
-$(document).on('click', '.date-picker .input', function (e) {
+$(".calendar2").datepicker({
+    dateFormat: 'mm/dd/yy',
+    firstDay: 1
+});
+
+$(document).on('click', '.date-picker1 .input1', function (e) {
     let $me = $(this),
-        $parent = $me.parents('.date-picker');
+        $parent = $me.parents('.date-picker1');
     $parent.toggleClass('open');
 
 });
 
-$(".calendar").on("change", function () {
+$(document).on('click', '.date-picker2 .input2', function (e) {
+    let $me = $(this),
+        $parent = $me.parents('.date-picker2');
+    $parent.toggleClass('open');
+
+});
+
+$(".calendar1").on("change", function () {
     let $me = $(this);
     let $selected = $me.val();
-    let $parent = $me.parents('.date-picker');
+    let $parent = $me.parents('.date-picker1');
 
     console.log($parent)
 
-    let results = document.querySelector(".selected-date");
+    
 
     console.log(results)
 
-    results.innerHTML = $parent.context.value;
+    results1.innerHTML = $parent.context.value;
 
 
 });
+
+$(".calendar2").on("change", function () {
+    let $me = $(this);
+    let $selected = $me.val();
+    let $parent = $me.parents('.date-picker2');
+
+    console.log($parent)
+
+    
+
+    console.log(results)
+
+    results2.innerHTML = $parent.context.value;
+
+
+
+});
+
+export { results1, results2 }
