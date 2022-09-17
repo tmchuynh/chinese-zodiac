c968a848e52a6d1f960988f9c10b6ff6ee9cc33a
Tina Huynh <tinamchuynh@gmail.com>
2022-09-17 14:18:56 -0700

    selection functionality

---
+++ calendar.js
@@ -0,0 +1,36 @@
+
+let select_date = document.querySelector(".result");
+$(function () {
+
+    select_date.innerHTML = "Select Date: "
+
+
+});
+
+$(".calendar").datepicker({
+    dateFormat: 'mm/dd/yy',
+    firstDay: 1
+});
+
+$(document).on('click', '.date-picker .input', function (e) {
+    let $me = $(this),
+        $parent = $me.parents('.date-picker');
+    $parent.toggleClass('open');
+
+});
+
+$(".calendar").on("change", function () {
+    let $me = $(this);
+    let $selected = $me.val();
+    let $parent = $me.parents('.date-picker');
+
+    console.log($parent)
+
+    let results = document.querySelector(".selected-date");
+
+    console.log(results)
+
+    results.innerHTML = $parent.context.value;
+
+
+});
