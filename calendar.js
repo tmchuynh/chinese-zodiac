196e74d349bf99752aab7b61f8cf681b5f995953
Tina Huynh <tinamchuynh@gmail.com>
2022-09-18 14:06:11 -0700

    Start calculations

--- calendar.js
+++ calendar.js
@@ -1,5 +1,5 @@
-var results1 = document.querySelector(".selected-date1");
-var results2 = document.querySelector(".selected-date2");
+const results1 = document.querySelector(".selected-date1");
+const results2 = document.querySelector(".selected-date2");
 
 $(".calendar1").datepicker({
     dateFormat: 'mm/dd/yy',
@@ -25,37 +25,50 @@ $(document).on('click', '.date-picker2 .input2', function (e) {
 
 });
 
-$(".calendar1").on("change", function () {
-    let $me = $(this);
-    let $selected = $me.val();
-    let $parent = $me.parents('.date-picker1');
-
-    console.log($parent)
-
+$('.calendar2',).on('click', function(e) {
     
+}), setInterval(50000);
 
-    console.log(results)
+$(".calendar1").on("change", function () {
+    let $me1 = $(this);
+    let $selected1 = $me1.val();
+    let $parent1 = $me1.parents('.date-picker1');
 
-    results1.innerHTML = $parent.context.value;
+    console.log($parent1)
+    console.log(results1)
+
+    results1.innerHTML = $parent1.context.value;
 
 
 });
 
 $(".calendar2").on("change", function () {
-    let $me = $(this);
-    let $selected = $me.val();
-    let $parent = $me.parents('.date-picker2');
+    let $me2 = $(this);
+    let $selected2 = $me2.val();
+    let $parent2 = $me2.parents('.date-picker2');
 
-    console.log($parent)
+    console.log($parent2)
+    console.log(results2)
 
-    
-
-    console.log(results)
-
-    results2.innerHTML = $parent.context.value;
-
-
+    results2.innerHTML = $parent2.context.value;
 
 });
 
-export { results1, results2 }
+// Put dates into storage
+$(".results").on("click", function() {
+    sessionStorage.setItem("results1", results1.innerHTML)
+    sessionStorage.setItem("results2", results2.innerHTML)
+})
+
+// Retrieve dates
+const date1 = sessionStorage.getItem("results1")
+const date2 = sessionStorage.getItem("results2")
+
+console.log(date1)
+console.log(date2)
+
+// Store dates into arrays
+const date1_m = date1.split("/")
+console.log(date1_m)
+const date2_m = date2.split("/")
+console.log(date2_m)
